b39d13009bf144db6b0d70aecccae2cdd2dfdf2d
96kuroguro <96kuroguro@gmail.com>
2017-08-22 14:28:50 +0900

    cmt

--- index.php
+++ index.php
@@ -2,56 +2,18 @@
 ini_set( 'display_errors', 1 );
 ini_set('error_reporting', E_ALL);
 
-define('YOUR_BOT_API_TOKEN', '442718912:AAFfm5yVq225XAsWMNRi7lifOVcRmFmsDjg');
-define('YOUR_BOTAN_TRACKER_API_KEY', 'fbfafc2b-2820-421f-b9d3-b4814c40bbbd');
+define('BOT_TOKEN', '442718912:AAFfm5yVq225XAsWMNRi7lifOVcRmFmsDjg');
+define('BOTAN_TRACKER_API_KEY', 'fbfafc2b-2820-421f-b9d3-b4814c40bbbd');
 
 require_once "vendor/autoload.php";
-try {
-    $bot = new \TelegramBot\Api\Client(YOUR_BOT_API_TOKEN, YOUR_BOTAN_TRACKER_API_KEY);
 
+use Telegram\Bot\Api;
 
-    $bot->command('ping', function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'pong!');
-    });
+$telegram = new Api('BOT_TOKEN');
 
-    $bot->command('', function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'on');
-    });
+$response = $telegram->getMe();
 
+$botId = $response->getId();
+$firstName = $response->getFirstName();
+$username = $response->getUsername();
 
-$bot->editedMessage(function ($message) use ($bot) {
-        //テキストを編集（EDIT）したときに動く
-        $bot->sendMessage($message->getChat()->getId(), 'editedMessage');
-});
-
-$bot->callbackQuery(function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'callbackQuery');
-});
-
-$bot->channelPost(function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'channelPost');
-});
-
-$bot->editedChannelPost(function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'editedChannelPost');
-});
-
-$bot->inlineQuery(function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'inlineQuery');
-});
-
-$bot->chosenInlineResult(function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'chosenInlineResult');
-});
-$bot->shippingQuery(function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'shippingQuery');
-});
-$bot->preCheckoutQuery(function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'preCheckoutQuery');
-});
-
-    $bot->run();
-
-} catch (\TelegramBot\Api\Exception $e) {
-    $e->getMessage();
-}
